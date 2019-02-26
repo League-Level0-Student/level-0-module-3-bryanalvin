f2ae0b1e20ce70e169ca7dc61f62465620129b7f
ilabstaff <ilabstaff@172.16.116.81>
2019-02-25 17:06:45 -0800

    I did birthday reminder and High low game

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -9,23 +9,39 @@ import javax.swing.JOptionPane;
 public class BirthdayReminder {
 
 	public static void main(String[] args) {
-	
+
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "April 5th";
+		String dadsBirthday = "June 30th";
+		String myBirthday = "May 13th";
 
-		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		// 2. Find out which birthday the user wants and and store their response in a
+		// variable
+		String Beat = JOptionPane.showInputDialog("Who's birthday do you want");
 		// 3. Print out what the user typed
-		
+		System.out.println(Beat);
 		// 4. if user asked for "mom"
-			//print mom's birthday
+		// print mom's birthday
+		if (Beat.equals("mom")) {
+			JOptionPane.showMessageDialog(null, momsBirthday);
+		}
 		// 5. if user asked for "dad"
-			// print dad's birthday
-		// 6. if user asked for your name
+		// print dad's birthday
+		if (Beat.equals("dad")) {
+			JOptionPane.showMessageDialog(null, dadsBirthday);
+			// 6. if user asked for your name
 			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
+		}
+		if (Beat.equals("Me")) {
 
-	} 
+			JOptionPane.showMessageDialog(null, myBirthday);
+		}
+		// 7. otherwise print "Sorry, i don't remember that person's birthday!"
+		else {
+			System.out.println(Beat);
+			JOptionPane.showMessageDialog(null, "Sorry i don't remember that persons birthday");
+		}
+
+	}
+
 }
