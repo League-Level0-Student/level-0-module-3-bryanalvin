8c6db07f1de55c6d44990d147e637043656c6cd2
ilabstaff <ilabstaff@172.16.116.79>
2019-03-04 17:32:08 -0800

    walk of fame and Dizzy robot it Works ohwow

--- src/extra/DizzyRobot.java
+++ src/extra/DizzyRobot.java
@@ -3,16 +3,19 @@
 
 package extra;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
-	static Robot robot = new Robot();
+	static Robot robot = new Robot("batman");
 	public static void main(String[] args) {
 		
 		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-
+		
+String Kris=JOptionPane.showInputDialog("On a scale from 1-10 how dizzy do you want to make the robot");
  // 1. Use the dance method to make the robot spin.
-	
+	dance(6);
 
 	}
 
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -8,22 +8,31 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		Robot Yugi = new Robot("batman");
+
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
-
+		Yugi.setX(0);
 		// 2. Make the robot draw a star shape. Hint: 144.
+		Yugi.penDown();
+		Yugi.setSpeed(100);
+		for (int j = 0; j < 10; j++) {
+			Yugi.setX(100*j);
+			for (int i = 0; i < 5; i++) {
+				Yugi.move(30);
+				Yugi.turn(144);
 
-		// 3. Set the size of the star to 30.
-
-		/** THE CHALLENGE: **/
-		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/RobotWalk
-		 * 
-		 * Hint: The distance between stars is 50.
-		 */
+			}
+		}
 
 	}
+	// 3. Set the size of the star to 30.
+
+	/** THE CHALLENGE: **/
+	/*
+	 * Make the robot draw a line of stars like this: http://bit.ly/RobotWalk
+	 * 
+	 * Hint: The distance between stars is 50.
+	 */
 
 }
